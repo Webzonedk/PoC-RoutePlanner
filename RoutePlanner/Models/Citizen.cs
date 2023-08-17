4520e1d44d8028cba9bb6b9259ab073ee420351a
Kent Pedersen <kp@soft7.dk>
2023-08-17 07:02:04 +0200

    Added CitizenID to Citicen class

--- RoutePlanner/Models/Citizen.cs
+++ RoutePlanner/Models/Citizen.cs
@@ -8,6 +8,7 @@ namespace RoutePlanner.Models
 {
     internal class Citizen
     {
+        public int ? CitizenID { get; set; }
         public string? CitizenName { get; set; }
         public int? ResidenceID { get; set; }
     }
